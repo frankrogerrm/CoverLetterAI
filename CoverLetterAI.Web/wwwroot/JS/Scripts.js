f4ef4c87df38373f3f82f8df2303a8be8195caae
frankrogerrm <framos@paradigmconsulting.com>
2025-05-03 09:10:44 -0600

    second commit

---
+++ CoverLetterAI.Web/wwwroot/JS/Scripts.js
@@ -0,0 +1,16 @@
+﻿
+async function openNewTabAndGetContent(url) {
+    alert(url);
+    return new Promise((resolve, reject) => {
+        var newTab = window.open(url, '_blank');
+        
+        //newTab.onload = function () {
+        //    var content = newTab.document.body.innerText;
+        //    newTab.close();
+        //    resolve(content);
+        //};
+        //newTab.onerror = function () {
+        //    reject("Failed to load the new tab.");
+        //};
+    });
+}
