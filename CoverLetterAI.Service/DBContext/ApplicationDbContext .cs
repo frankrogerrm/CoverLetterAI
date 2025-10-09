7c70e41f347aa251282fd0263b4fa38750cd7d01
frankrogerrm <framos@paradigmconsulting.com>
2025-10-09 12:58:41 +0200

    second commit

---
+++ CoverLetterAI.Service/DBContext/ApplicationDbContext .cs
@@ -0,0 +1,16 @@
+﻿using CoverLetterAI.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoverLetterAI.Service.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicantProfile> ApplicantProfiles { get; set; }
+
+
+    }
+}
---
+++ CoverLetterAI.Service/Entities/ApplicantProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoverLetterAI.Service.Entities
+{
+    [Table("ApplicantProfile")]
+    public class ApplicantProfile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public string FullAddress { get; set; } = string.Empty;
+        public string Profession { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string MyBackground { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int UserId { get; set; }
+    }
+}
